c17444a12112bee0d9583ac0d7c8a718100b632a
Rabb1t <temka.dust20@gmail.com>
2019-10-13 10:28:48 +0300

    added first worked version

---
+++ SimpleDeleteFiles/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleDeleteFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Console.Write("Введите/вставьте путь к папке: ");
+            string sourceDirectory = Console.ReadLine();
+
+            Console.Write("Введите имя расширения для удаление (* - все файлы): ");
+            string patternFind = Console.ReadLine();
+
+            var files = Directory.GetFiles(sourceDirectory, patternFind, SearchOption.TopDirectoryOnly);
+
+            Console.WriteLine("Будут удалены следующие файлы:");
+            foreach (var file in files)
+            {
+                Console.WriteLine($"  -> {file}");
+            }
+
+            ConsoleKey key;
+            do
+            {
+                Console.WriteLine("Подтвердите изменения нажатием на кнопку Enter.");
+                key = Console.ReadKey().Key;
+            } while (key != ConsoleKey.Enter);
+
+            Console.WriteLine("Выполнение...");
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
---
+++ SimpleDeleteFiles/bin/Debug/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDeleteFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Console.Write("Введите/вставьте путь к папке: ");
+            string sourceDirectory = Console.ReadLine();
+
+            Console.Write("Введите имя расширения для удаление (* - все файлы): ");
+            string patternFind = Console.ReadLine();
+
+            var files = Directory.GetFiles(sourceDirectory, patternFind, SearchOption.TopDirectoryOnly);
+
+            Console.WriteLine("Будут удалены следующие файлы:");
+            foreach (var file in files)
+            {
+                Console.WriteLine($"  -> {file}");
+            }
+
+            ConsoleKey key;
+            do
+            {
+                Console.WriteLine("Подтвердите изменения нажатием на кнопку Enter.");
+                key = Console.ReadKey().Key;
+            } while (key != ConsoleKey.Enter);
+
+            Console.WriteLine("Выполнение...");
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
